96a10c4868ed68ac573b92abd5f5a335af40de87
Finkch <skyler@thealdersons.org>
2022-02-27 11:08:09 -0800

    Made a working version
    Stitched together Robert's file reader with some basic start up stuff. Also fixed the file reader (scope issue with hashMap2) as well as several typos in the Dictionary.txt (namely ":" that improperly delimited). Also turned the file reader into a class with a few functions, namely one to return the Dictionary and another to return the Keys and their synonyms.
    
    To play around with this version, run the program and type into the console. You'll need two lines to elicit a response: the first is the Important Noun, and the second is the W.
    
    I haven't done anything to parse user input with any intelligence, so we still need to hook that up.

---
+++ src/Startup.java
@@ -0,0 +1,93 @@
+
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Startup {
+	
+	
+	//	Starts the program up, then enters the primary loop
+	public static void main(String args[]) {
+		
+		// This section will have everything required for startup
+		//		Which includes the loading of the Dictionary
+		
+		//	Reads the dictionary files and puts each entry into a HashMap
+		HashMap<String, ArrayList<String>> 			keys = readFile.loadKeys("./src/Dictionary");
+		HashMap<String, HashMap<String, String>>	dict = readFile.loadDict("./src/Dictionary");
+		
+		
+		
+		//	An example of how to fetch keys
+		//		keys.keySet();
+		
+		//	An example of how to fetch a key's synonyms
+		//		keys.get("help");
+		
+		
+		//	Enters the primary loop
+		mainLoop(dict);
+		
+	}
+	
+	
+	//	This loop will call functions to read input and produce output
+	public static void mainLoop(HashMap<String, HashMap<String, String>> dict) {
+		
+		
+		//	Used to know when to exit the primary loop
+		boolean go = true;
+		
+		
+		//	Scanner temp
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		//	The primary loop
+		while(go) {
+			
+			
+			//	Does some formatting
+			System.out.print("User > ");
+			
+			
+			//	Fetches the keys from the user's input
+			String keyOne = input.nextLine();
+			String keyTwo = input.nextLine();
+			
+			//	If the user only enters one key, sets the second to be "generic"
+			if(keyTwo == "")
+				keyTwo = "generic";
+			
+			
+			//	Fetches the appropriate response
+			String response = fetchResponse(keyOne, keyTwo, dict);
+			
+			
+			//	Prints the response out
+			System.out.println(response);
+			
+		}
+		
+		
+		//	Closes the input
+		input.close();
+		
+	}
+	
+	
+	public static String fetchResponse(String keyOne, String keyTwo, HashMap<String, HashMap<String, String>> dict) {
+		
+		
+		String out = "\nOS > ";
+		
+		if(dict.get(keyOne) == null)
+			return out + "Sorry, I didn't understand \"" + keyOne + "\".";
+		
+		if(dict.get(keyOne).get(keyTwo) == null)
+			return out + "Sorry, I didn't understand \"" + keyTwo + "\".";
+		
+		return out + dict.get(keyOne).get(keyTwo);
+	}
+}
---
+++ src/readFile.java
@@ -0,0 +1,145 @@
+
+import java.util.*;
+import java.io.*;
+
+public class readFile {
+
+	 public static HashMap<String, HashMap<String, String>> loadDict(String filePath) {
+		    try {
+		      File myObj = new File(filePath);
+		      Scanner myReader = new Scanner(myObj);
+		      
+		      //Second HashMap necessary to determine what response the user will get based on the Key Value and the who/what/when/where/why/how and generic response
+		      HashMap<String, HashMap<String, String>> hashMap2 = new HashMap<String, HashMap<String, String>>();
+		      
+		      String temptemp = "";
+		      
+	      	  String st = myReader.nextLine();
+		      
+		      while(myReader.hasNextLine()) {
+		    	  
+		    	  if(st.charAt(0) != '\t') {
+		    		  
+		    		  //	Splits the read string to separate key values and user inputs
+		    		  String[] split = st.split(":");
+		    		  
+		    		  //	Stores the Key and the potential user inputs into the Hash Map
+		    		  temptemp = split[0];
+		    		  
+			      	  st = myReader.nextLine();  
+		    	  }
+		    	  else {
+		    		  
+		    		  HashMap<String, String> responses = new HashMap<String, String>();
+		    		  
+		    		  //	This while loop is to fix a scope problem that previously existed
+		    		  //		The inner hashmap needs to hold responses for every 'W'
+		    		  //		The previous implementation re-assigned the hashmap for every 'W'
+		    		  //		Thus previously, only the final 'W' was included
+		    		  while(st.charAt(0) == '\t') {
+		    			  
+		    			  if(!myReader.hasNextLine())
+		    				  break; 
+		    				  
+			    		  String[] split2 = st.split(":");
+			    		  
+			    		  String wwwyh = split2[0];
+			    		  wwwyh = wwwyh.replace("\t", "");
+			    		  
+			    		  String response = split2[1];
+			    		  
+			    		  //	Removes the leading white space
+			    		  response = response.substring(1);
+			    		  
+			    		  //	Removes the quotations marks
+			    		  for(int i = 0; i < response.length(); i++)
+			    			  if(response.charAt(i) == '"') {
+			    				  response = response.substring(0, i) + response.substring(i + 1, response.length());
+			    				  i--;
+			    			  }
+			    		  
+			    		  //	Appends a period to the response
+			    		  //		First item in the if statement is to prevent an empty string from throwing an error
+			    		  if(response.length() > 1 && response.charAt(response.length() - 1) != '.')
+			    			  response += '.';
+			    		  
+			    		  
+			    		  //	Places the response into the inner hash mao
+			    		  responses.put(wwwyh, response);
+			    		  
+			    		  
+			    		  //	Calls for the next line
+			    		  st = myReader.nextLine();
+		    		  }
+		    		  
+		    		  
+		    		  //	Places the inner hash map into the outer one
+		    		  hashMap2.put(temptemp, responses);
+		    		  
+		    		  
+		    	  }
+		      }
+		      
+		      myReader.close();
+		      
+			  return hashMap2;
+		      
+		      
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred: " + e);
+		      e.printStackTrace();
+		      return null;
+		    }
+		    
+	 }
+	 
+	 
+	 //	Returns a hashmap of key values and their synonyms 
+	 public static HashMap<String, ArrayList<String>> loadKeys(String filePath) {
+		    try {
+		      File myObj = new File(filePath);
+		      Scanner myReader = new Scanner(myObj);
+		      
+		      //First HashMap necessary to parse the users inputs for values the will relate to the Key Value
+		      HashMap<String, ArrayList<String>> hashMap1 = new HashMap<String, ArrayList<String>>();
+		      
+		      
+	      	  String st = myReader.nextLine();
+		      
+		      while(myReader.hasNextLine()) {
+		    	  
+		    	  if(st.charAt(0) != '\t') {
+		    		  
+		    		  //	Splits the read string to separate key values and user inputs
+		    		  String[] split = st.split(":");
+		    		  
+		    		  
+		    		  //	Splits the second string by commas and stores values as a list
+		    		  //		This method is to remove any leading whitespace that might be present
+		    		  String[] synonyms = split[1].split(",");
+		    		  ArrayList<String> userInputs = new ArrayList<String>();
+		    		  for(String str : synonyms)
+		    			  userInputs.add(str.stripLeading());
+		    		  
+		    		  
+		    		  //	Stores the Key and the potential user inputs into the Hash Map
+		    		  hashMap1.put(split[0], userInputs);
+
+		    	  }
+		    	  
+		    	  st = myReader.nextLine();
+		      }
+		      
+		      myReader.close();
+		      
+			  return hashMap1;
+		      
+		      
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred: " + e);
+		      e.printStackTrace();
+		      return null;
+		    }
+		    
+	 }
+}
