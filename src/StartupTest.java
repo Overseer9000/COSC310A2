5e6837cb4bec3a1d73d84f5602d993764206f3e5
Finkch <skyler@thealdersons.org>
2022-03-09 16:20:03 -0800

    Finished JUnit for Startup
    Added some more test cases, mainly involving cases where the keys existed. Also removed the dependency to the ReadFile class, and created a custom DIctionary for this test

--- src/StartupTest.java
+++ src/StartupTest.java
@@ -12,25 +12,38 @@ class StartupTest {
 	@Test
 	void testFetchResponse() {		
 		
-		//	I feel like this isn't proper practice with JUnit test, but I'm not sure of a better way
-		HashMap<String, HashMap<String, String>> dict = ReadFile.loadDict("./src/Dictionary.txt");
-		String generic = "generic";
+		//	Consctructs a Dictionary
+		HashMap<String, HashMap<String, String>> dict = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> inner = new HashMap<String, String>();
+		inner.put("generic", "Birds are cool!");
+		inner.put("what", "A bird is an avian creature descended from reptiles.");
+		inner.put("where", "There are no birds aboard the Blue Dwarf.");
+		dict.put("bird", inner);
 		
 		
-		//	Test the generic case
-		assertEquals("\nOS > Hm, it seems I was unable to understand what you said.", Startup.fetchResponse(generic, generic, dict));
+		
+		//	Test some cases where a key is incorrect
+		//		Test the generic case
+		assertEquals("\nOS > Hm, it seems I was unable to understand what you said.", Startup.fetchResponse("generic", "generic", dict));
 		
 		
-		//	Tests an unknown key one
+		//		Tests an undefined key one
 		assertEquals("\nOS > Sorry, I didn't understand \"verisimilitude\".", Startup.fetchResponse("verisimilitude", "generic", dict));
 		
 		
-		//	Tests an unknown key two
-		assertEquals("\nOS > Sorry, I didn't understand \"sociopath\".", Startup.fetchResponse("os", "sociopath", dict));
+		//		Tests an undefined key two
+		assertEquals("\nOS > Sorry, I didn't understand \"sociopath\".", Startup.fetchResponse("bird", "sociopath", dict));
 		
 		
-		//	Tests two unknown 
+		//		Tests two undefined keys 
+		assertEquals("\nOS > Sorry, I didn't understand \"dinosaurs\".", Startup.fetchResponse("dinosaurs", "lame", dict));
 		
+		
+		
+		//	Test a few cases where the keys are correct
+		assertEquals("\nOS > Birds are cool!", Startup.fetchResponse("bird", "generic", dict));
+		assertEquals("\nOS > A bird is an avian creature descended from reptiles.", Startup.fetchResponse("bird", "what", dict));
+		assertEquals("\nOS > There are no birds aboard the Blue Dwarf.", Startup.fetchResponse("bird", "where", dict));
 		
 	}
 
